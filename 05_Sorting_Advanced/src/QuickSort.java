6526c11c780c03071fb3f34dc0762a193707a8d5
mulandi <mulandimorris1@gmail.com>
2026-02-16 23:51:54 +0300

    feat: Implement basic sorting algorithms and a verification script, add initial ADT documentation, and remove existing advanced sort implementations.

--- 05_Sorting_Advanced/src/QuickSort.java
+++ 05_Sorting_Advanced/src/QuickSort.java
@@ -1,37 +1,5 @@
-package src;
-
-import java.util.Arrays;
+package 05_Sorting_Advanced.src;
 
 public class QuickSort {
- public static int partition(int[] arr, int low, int high) {
-  int pivot = arr[high];
-  int i = (low - 1);
-  for (int j = low; j < high; j++) {
-   if (arr[j] < pivot) {
-    i++;
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-   }
-  }
-  int temp = arr[i + 1];
-  arr[i + 1] = arr[high];
-  arr[high] = temp;
-  return i + 1;
- }
-
- public static void sort(int[] arr, int low, int high) {
-  if (low < high) {
-   int pi = partition(arr, low, high);
-   sort(arr, low, pi - 1);
-   sort(arr, pi + 1, high);
-  }
- }
-
- public static void main(String[] args) {
-  int[] data = { 10, 7, 8, 9, 1, 5 };
-  System.out.println("Unsorted: " + Arrays.toString(data));
-  sort(data, 0, data.length - 1);
-  System.out.println("Sorted: " + Arrays.toString(data));
- }
+ 
 }
